654f0d17bdd6d183a607e8ebf56e263951e23cd0
Mehdi ElHaij <mehdi.elhaij@outlook.com>
2021-04-10 16:34:33 +0200

    multi coin

--- main.py
+++ main.py
@@ -12,9 +12,21 @@ BINANCE_SECRET_KEY = os.getenv("BINANCE_SECRET_KEY")
 
 binance_client = Client(BINANCE_API_KEY, BINANCE_SECRET_KEY)
 
-pair='ENJUSDT'
+# pair='ENJUSDT'
 str_interval='30s'
 interval=timedelta(seconds=30)
-s = DualMovingAvgs(name="DualMovingAvgs_{}_{}".format(pair, str_interval), binance_client=binance_client, pair=pair, initial_capital=100)
 
-s.run(interval)
+
+FIAT = 'USDT'
+COINS = ['ENJ', 'DOGE', 'BTT'] 
+
+pairs = [coin+FIAT for coin in COINS]
+
+bots = []
+for pair in pairs:
+  bot = DualMovingAvgs(name="DualMovingAvgs_{}_{}".format(pair, str_interval), binance_client=binance_client, pair=pair, initial_capital=100)
+  bot.run(interval)
+  bots.append(bot)
+
+print('Main:', len(bots), 'bots running')
+# s.run(interval)
